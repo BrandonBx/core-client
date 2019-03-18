f0331a1e59b9a274d8cf7b2fed23007aaa3261c5
Brandon Buxin <bubr@sbim.be>
2019-03-18 15:38:13 +0100

    Import FlexLayoutModule and update angular core + cli v6 to v7

--- core-client/src/shared/shared.module.ts
+++ core-client/src/shared/shared.module.ts
@@ -2,21 +2,25 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatCardModule, MatFormFieldModule, MatInputModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
-const MATERIAL_MODULES = [
+const modules = [
   BrowserAnimationsModule,
   MatInputModule,
   MatFormFieldModule,
   MatCardModule,
+  FlexLayoutModule,
 ];
 
 @NgModule({
   imports: [
     CommonModule,
-    ...MATERIAL_MODULES,
+    ...modules,
   ],
   declarations: [],
-  exports: [...MATERIAL_MODULES],
+  exports: [
+    ...modules,
+  ],
 })
 export class SharedModule {
 }
