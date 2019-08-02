980eb5a9b83ab267287f0ffe3818000ddcab276c
Brandon Buxin <bubr@sbim.be>
2019-08-02 13:44:43 +0200

    Refactoring + Migrating nativescript and angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {LoginComponent} from './login/login.component';
-
-const ROUTES = [
-  {path: 'login', component: LoginComponent},
-  {path: '', redirectTo: '/login', pathMatch: 'full'}
-];
+import { ROUTES } from './app.routes';
 
 @NgModule({
   imports: [RouterModule.forRoot(ROUTES)],
